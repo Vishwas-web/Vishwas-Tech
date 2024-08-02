465138a0a3efdb34255c4a3db42dc9012fdd1956
Vishwas-web <vishalpatil3833@gmail.com>
2024-08-02 17:55:07 +0530

    Code

---
+++ src/Details.js
@@ -0,0 +1,180 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import selenium from "./assets/techstack/selenium.jpg";
+import TestNG from "./assets/techstack/TestNG.png";
+import js from "./assets/techstack/js.png";
+import java from "./assets/techstack/java.png";
+import react from "./assets/techstack/react.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+import ExtentReport from "./assets/techstack/ExtentReport.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "VISHWAS PATIL",
+  tagline: "Your Web & App Testing Ace.",
+  img: profile,
+  about: `As a software testing and UI/UX enthusiast, I bring a wealth of experience and passion to every project I take on. My focus on quality assurance, attention to detail, and commitment to delivering exceptional user experiences have allowed me to excel in my field. With a strong foundation in both software testing and UI/UX design principles, I am able to bridge the gap between technical requirements and user needs to deliver innovative and effective solutions.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/vishwas-patil-25b847192/",
+  github: "https://github.com/Vishwas-web",
+  twitter: "https://twitter.com/",
+  //instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Jr.Software Tester",
+    Company: `KCS`,
+    Location: "Chh. Sambhaji Nagar",
+    Type: "Full Time",
+    Duration: "Oct 2023 - Present",
+  },
+  {
+    Position: "Software Tester Intern(Training)",
+    Company: `Cyber Success`,
+    Location: "Pune",
+    Type: "Internship",
+    Duration: "Mar 2023 - Aug 2023",
+  },
+  {
+    Position: "UI/UX",
+    Company: `Neyx`,
+    Location: "Remote",
+    Type: "Internship",
+    Duration: "Oct 2022 - Dec 2022",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "B.Tech-ENTC",
+    Company: "Deogiri Institute Of Engineering and Management Studies",
+    Location: "Chh. Sambhaji Nagar",
+    Type: "8.74 CGPA",
+    Duration: "Aug 2019 - Jun 2023",
+  },
+  // {
+  //   Position: "Bachelor in Electronics & Communication",
+  //   Company: `Your College Name here`,
+  //   Location: "Bengaluru",
+  //   Type: "Full Time",
+  //   Duration: "Aug 2020 - Present",
+  // },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  selenium:selenium,
+  TestNG:TestNG,
+  java:java,
+  ExtentReport:ExtentReport,
+  //redux: redux,
+  //sass: sass,
+  //tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "ActiTime",
+    image: projectImage1,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "Manaul Testing, Automation Testing",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Hotel Management",
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 3",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 4",
+    image: projectImage4,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 5",
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 6",
+    image: projectImage6,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "Vishwaspatil9146@gmail.com",
+  phone: "Connect with on social 👆",
+};
