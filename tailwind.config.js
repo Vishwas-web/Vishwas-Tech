b78d0e984d2070b16cd601923ed2c05c6ba1b3b2
Vishwas <66787592+Vishwas-web@users.noreply.github.com>
2024-08-03 08:26:45 +0000

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,14 @@ module.exports = {
         primary: "Poppins, sans-serif",
       },
       colors: {
-        "light-content": "#A7A7A7",
-        "dark-heading": "#1f2428",
-        "dark-content": "#666666",
-        "light-heading": "#CCCCCC",
-        "dark-mode": "#191919",
-        "dark-card": "#363636",
-        "green-text": "#018C0F",
-        "greenbg": "#D7FFE0",
+        "light-content": "Black", // Changes the Header Color
+        "dark-heading": "Black",
+        "dark-content": "Black", // Changes the Text
+        "light-heading": "Black",
+        "dark-mode": "White",
+        "dark-card": "Black",
+        "green-text": "White",
+        "greenbg": "Black",
       },
     },
   },
