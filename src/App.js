f46089991d70b0d91e061c3578518e715ef8ed06
Vishwas <66787592+Vishwas-web@users.noreply.github.com>
2024-09-27 07:29:55 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { HashRouter as Router, Routes, Route } from "react-router-dom"; // Change to HashRouter
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { HashRouter as Router, Routes, Route } from "react-router-dom"; // Change to HashRouter
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Home from "./Pages/Home";
