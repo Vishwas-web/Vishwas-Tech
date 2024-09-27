659bf07c870a6e8eb5f37a488cd87aed01a4ab83
Vishwas <66787592+Vishwas-web@users.noreply.github.com>
2024-09-27 07:02:41 +0000

    Update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";  // Import BrowserRouter
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router basename={process.env.PUBLIC_URL}>  {/* Set basename */}
+  <Router basename={process.env.PUBLIC_URL}>
     <App />
   </Router>
 );
